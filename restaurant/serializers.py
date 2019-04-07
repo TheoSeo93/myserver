849c1d4b4a108c87fd29a77fbac53dd1e36f056a
Theo <youngho.seo@stonybrook.edu>
2019-04-07 08:25:20 -0400

    Comment Typo

--- restaurant/serializers.py
+++ restaurant/serializers.py
@@ -31,13 +31,13 @@ class ItemSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         """
-        Create and return a new `Snippet` instance, given the validated data.
+        Create and return a new `Section` instance, given the validated data.
         """
         return Item.objects.create(**validated_data)
 
     def update(self, instance, validated_data):
         """
-        Update and return an existing `Snippet` instance, given the validated data.
+        Update and return an existing `Section` instance, given the validated data.
         """
         instance.name = validated_data.get('name', instance.menuName)
         instance.save()
