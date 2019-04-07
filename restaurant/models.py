6ae9853bde6a94b48cd4f9296d73aaddebf50956
Theo <youngho.seo@stonybrook.edu>
2019-04-07 08:50:25 -0400

    id : AutoField -> BigIntegerField

--- restaurant/models.py
+++ restaurant/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class MenuSection(models.Model):
-    id = models.AutoField(primary_key=True)
+    id = models.BigIntegerField(primary_key=True)
     name = models.CharField(max_length=20)
 
     def __str__(self):
